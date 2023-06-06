81760d000b82607b671a7d32b198346cf0d676b9
Camilo <jclopez.9708@gmail.com>
2023-06-05 23:10:44 -0500

    configuracion .env

--- src/database/config.js
+++ src/database/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-        await mongoose.connect( process.env.DB_CNN_TEST , {
+        await mongoose.connect( process.env.DB_CNN , {
             useNewUrlParser: true, 
             useUnifiedTopology: true,
         });
